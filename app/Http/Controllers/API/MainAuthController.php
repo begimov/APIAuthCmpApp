9e32a8995640838d69541289052f090ff6e2c7c5
begimov <begimov@gmail.com>
2018-01-31 18:41:09 +0300

    refresh method to refresh access_tokens implemented in MainAuthController

--- app/Http/Controllers/API/MainAuthController.php
+++ app/Http/Controllers/API/MainAuthController.php
@@ -51,4 +51,27 @@ class MainAuthController extends Controller
 
         return redirect('/home');
     }
+
+    public function refresh(Request $request)
+    {
+        $res = $this->guzzleClient->post('http://apiauthcmp.test/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $request->user()->token->refresh_token,
+                'client_id' => '3',
+                'client_secret' => 'rUJ2UWJzh3HNGsOjXHcjZVwyngZDna6nMZVjDzqb',
+                'scope' => 'view-messages'
+            ]
+        ]);
+
+        $res = json_decode($res->getBody());
+
+        $request->user()->token()->update([
+            'access_token' => $res->access_token,
+            'refresh_token' => $res->refresh_token,
+            'expires_in' => $res->expires_in,
+        ]);
+
+        return redirect()->back();
+    }
 }
